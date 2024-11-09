ead97d8d907124738448fa765555bdc070f0e6d4
weeaa <github@ryanriviere.com>
2024-11-09 22:31:11 +0100

    added RetrieveBundleIDfromTransactionSignature method

--- api/api.go
+++ api/api.go
@@ -5,14 +5,10 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"net/url"
-)
-
-type method string
-
-var (
-	getBundleHistory method = "getBundleHistory"
 )
 
 type Client struct {
@@ -24,12 +20,44 @@ func New(ctx context.Context, client *http.Client) *Client {
 	return &Client{ctx: ctx, c: client}
 }
 
-func (api *Client) RetrieveBundleIDfromTransaction() error {
-	return errors.New("not impl yet")
+// RetrieveBundleIDfromTransactionSignature returns the bundleID associated with the transaction signature provided, if existing.
+func (api *Client) RetrieveBundleIDfromTransactionSignature(signature string) (string, error) {
+	req := &http.Request{
+		Method: http.MethodGet,
+		URL: &url.URL{
+			Scheme: "https",
+			Host:   "bundles.jito.wtf",
+			Path:   fmt.Sprintf("/api/v1/bundles/transaction/%s", signature),
+		},
+		Header: headers.Clone(),
+	}
+
+	resp, err := api.c.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("error retrieving bundle ID from transaction: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("error getting bundle id: %s", resp.Status)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var out []map[string]string
+	if err = json.Unmarshal(body, &out); err != nil {
+		return "", err
+	}
+
+	return out[0]["bundle_id"], nil
 }
 
 type tmp struct{}
 
+/*
 func (api *Client) DoYeet() (*tmp, error) {
 	panic("impl me pls")
 	//https://explorer.jito.wtf/wtfrest/api/v1/bundles/recent?limit=1000&sort=Tip&asc=false&timeframe=Week
@@ -55,9 +83,12 @@ func (api *Client) DoYeet() (*tmp, error) {
 
 	return nil, nil
 }
+*/
 
 // RetrieveRecentBundles fetches a list of recent bundles from the Jito API within a specified timeframe and limit.
 func (api *Client) RetrieveRecentBundles(limit int, timeFrame Timeframe) (*RecentBundlesResponse, error) {
+	return nil, errors.New("i'm broken pls wait ser till im fixed")
+
 	params := &url.Values{
 		"limit":     {fmt.Sprint(limit)},
 		"sort":      {"Time"},
@@ -75,6 +106,7 @@ func (api *Client) RetrieveRecentBundles(limit int, timeFrame Timeframe) (*Recen
 		},
 		Header: headers.Clone(),
 	}
+	log.Println(req)
 
 	resp, err := api.c.Do(req)
 	if err != nil {
@@ -94,8 +126,8 @@ func (api *Client) RetrieveRecentBundles(limit int, timeFrame Timeframe) (*Recen
 	return &bundles, nil
 }
 
+// GetBundleInfo returns information associated with a bundle ID.
 func (api *Client) GetBundleInfo(bundleID string) (*GetBundleInfoResponse, error) {
-
 	req := &http.Request{
 		Method: http.MethodPost,
 		URL: &url.URL{
@@ -124,4 +156,6 @@ func (api *Client) GetBundleInfo(bundleID string) (*GetBundleInfoResponse, error
 	return &bundleInfo, nil
 }
 
-func GetLargestBundlesBySigner() {}
+func (api *Client) GetLargestBundlesBySigner() error {
+	return errors.New("pls impl me")
+}
--- api/api_test.go
+++ api/api_test.go
@@ -1,20 +1,32 @@
 package api
 
 import (
+	"context"
 	"log"
+	"net/http"
 	"testing"
 )
 
+// todo add more tests
 func TestApi(t *testing.T) {
-	client := New()
-	resp, err := client.RetrieveRecentBundles(10, Day)
-	if err != nil {
-		t.Fatal(err)
-	}
-	
-	log.Println(resp)
-	
-	mot := "bonjour"
-	
-	client.RetrieveRecentBundles(1, Timeframe(mot))
+	ctx := context.Background()
+	client := New(ctx, &http.Client{})
+
+	t.Run("RetrieveBundleIDfromTransactionSignature", func(t *testing.T) {
+		bundleID, err := client.RetrieveBundleIDfromTransactionSignature("25buYcoQm4BtjPPyk1VTdjFr6fKVBRSBwg5zFLcocLUkJYrd1HQTwmWCgNGtfJ2p5Gu1LF3rmwLaWySFtJJ7jALd")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		log.Printf("bundleID from signature: %s", bundleID)
+	})
+
+	t.Run("RetrieveRecentBundles", func(t *testing.T) {
+		resp, err := client.RetrieveRecentBundles(10, Day)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		log.Printf("recent bundles: %+v", resp)
+	})
 }
